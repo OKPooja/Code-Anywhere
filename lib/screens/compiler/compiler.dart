13db1b93e384f35b5695a4ff525a9c34a6980a3a
OKPooja <pooja.gawade213046@tsecol.onmicrosoft.com>
2024-02-24 02:11:55 +0530

    added share button, back button and three dots in the compiler screen

--- lib/screens/compiler/compiler.dart
+++ lib/screens/compiler/compiler.dart
@@ -1,8 +1,12 @@
 import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:highlight/languages/dart.dart';
 import 'package:highlight/languages/python.dart';
+import 'package:highlight/languages/cpp.dart';
+import 'package:highlight/languages/java.dart';
 import 'package:flutter_highlight/themes/monokai-sublime.dart';
 import 'package:flutter_highlight/themes/vs.dart';
 import 'package:flutter_highlight/themes/atom-one-dark.dart';
@@ -20,46 +24,92 @@ class CodeInputScreen extends StatefulWidget {
 class _CodeInputScreenState extends State<CodeInputScreen> {
   CodeController? _codeController;
   Map<String, TextStyle>? theme = monokaiSublimeTheme;
+  String _selectedLanguage = 'C++';
 
   @override
   Widget build(BuildContext context) {
-    const source = "void main() {\n    print(\"Hello, world!\");\n}";
+    const source = "void main() {\n    cout<<\"Hello, world!\";\n}";
     _codeController = CodeController(
       text: source,
-      language: dart,
+      language: cpp,
       patternMap: {
         r'".*"': const TextStyle(color: Colors.white),
         r'[a-zA-Z0-9]+\(.*\)': const TextStyle(color: Colors.white),
       },
       stringMap: {
         "void": const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFEF9A9A)),
-        "print": const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFFFF9C4)),
+        "cout": const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFFFF9C4)),
       },
     );
 
     return Scaffold(
-      backgroundColor: Color(0xFF212121) ,
+      backgroundColor: Color(0xFF212121),
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Get.back();
+          },
+        ),
         backgroundColor: AppColors.primary,
         title: Text(
-            "Code",
-            style: TextStyle(
-            fontFamily: 'Nunito',
+          "Code",
+          style: TextStyle(
+            fontFamily: 'PragatiNarrow',
             fontSize: 30.sp,
             fontWeight: FontWeight.w500,
-            color: Colors.white
+            color: Colors.white,
           ),
         ),
+        actions: [
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Icon(
+                Icons.share,
+                color: Colors.white,
+              ),
+            ),
+          PopupMenuButton<String>(
+            color: Colors.white,
+            onSelected: (String value) {
+              setState(() {
+                _selectedLanguage = value;
+                if (value == 'C++') {
+                  _codeController?.language = cpp;
+                } else if (value == 'Java') {
+                  _codeController?.language = java;
+                } else if (value == 'Python') {
+                  _codeController?.language = python;
+                }
+              });
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'C++',
+                child: Text('C++'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Java',
+                child: Text('Java'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Python',
+                child: Text('Python'),
+              ),
+            ],
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: CodeField(
-          //background: Colors.white10,
           controller: _codeController!,
           cursorColor: AppColors.primary,
           textStyle: const TextStyle(fontFamily: 'Nunito', fontSize: 20,),
-          // lineNumberStyle: const LineNumberStyle(
-          //     textStyle: TextStyle(fontFamily: 'Nunito', fontSize: 20, color: Colors.black),
-          // ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -22,258 +22,256 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        //backgroundColor: Colors.white,
-        backgroundColor: AppColors.primary,
-          body: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 20.h,
-                  ),
-
-              ///Appbar
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.w,
+    return Scaffold(
+      //backgroundColor: Colors.white,
+      backgroundColor: AppColors.primary,
+        body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50.h,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hey User!',
+
+            ///Appbar
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.w,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hey User!',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 38.sp,
+                            color: Colors.white,
+                            fontFamily: 'PragatiNarrow'),
+                      ),
+                      Text(
+                        'Let’s start learning!',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 22.sp,
+                            color: Colors.black,
+                            fontFamily: 'Nunito'),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    'assets/home/dummy_profile.png',
+                    width: 80.w,
+                    height: 90.h,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+
+            ///Categories
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.w,
+              ),
+              child: Text(
+                'Categories',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 38.sp,
+                    color: Colors.white,
+                    fontFamily: 'PragatiNarrow'),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.w,
+              ),
+              child: SizedBox(
+                height: 250.h,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: 10.w, left: 10.w),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 150.w,
+                            height: 150.h,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: AssetImage(categories[index].image),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10.h),
+                          Text(
+                            categories[index].title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18.sp,
+                                color: Colors.white,
+                                fontFamily: 'PragatiNarrow'),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Container(
+              height: 550,
+              width: double.infinity,
+              decoration:  BoxDecoration(
+                //color: AppColors.primary,
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Text(
+                  //     'Topics',
+                  //     style: TextStyle(
+                  //         fontWeight: FontWeight.w700,
+                  //         fontSize: 30.sp,
+                  //         color: Colors.black,
+                  //         fontFamily: 'PragatiNarrow',
+                  //     ),
+                  //   ),
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  Stack(
+                    children: [
+                      Image.asset(
+                        'assets/home/card.png',
+                        width: 330.w,
+                      ),
+                      Positioned(
+                        left: 20.w,
+                        top: 15.h,
+                        child: Text(
+                          'Arrays',
                           style: TextStyle(
                               fontWeight: FontWeight.w700,
-                              fontSize: 38.sp,
+                              fontSize: 45.sp,
                               color: Colors.white,
                               fontFamily: 'PragatiNarrow'),
                         ),
-                        Text(
-                          'Let’s start learning!',
+                      ),
+                      Positioned(
+                        left: 20.w,
+                        top: 60.h,
+                        child: Text(
+                          'Start learning',
                           style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 22.sp,
-                              color: Colors.black,
-                              fontFamily: 'Nunito'),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25.sp,
+                              color: Colors.white,
+                              fontFamily: 'PragatiNarrow'),
                         ),
-                      ],
-                    ),
-                    Image.asset(
-                      'assets/home/dummy_profile.png',
-                      width: 80.w,
-                      height: 90.h,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-
-              ///Categories
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.w,
-                ),
-                child: Text(
-                  'Categories',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 38.sp,
-                      color: Colors.white,
-                      fontFamily: 'PragatiNarrow'),
-                ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.w,
-                ),
-                child: SizedBox(
-                  height: 250.h,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: categories.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.only(right: 10.w, left: 10.w),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 150.w,
-                              height: 150.h,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                image: DecorationImage(
-                                  image: AssetImage(categories[index].image),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 10.h),
-                            Text(
-                              categories[index].title,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 18.sp,
-                                  color: Colors.white,
-                                  fontFamily: 'PragatiNarrow'),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+                      )
+                    ],
                   ),
-                ),
-              ),
-              Container(
-                height: 550,
-                width: double.infinity,
-                decoration:  BoxDecoration(
-                  //color: AppColors.primary,
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
+                  SizedBox(
+                    height: 20.h,
                   ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    // Text(
-                    //     'Topics',
-                    //     style: TextStyle(
-                    //         fontWeight: FontWeight.w700,
-                    //         fontSize: 30.sp,
-                    //         color: Colors.black,
-                    //         fontFamily: 'PragatiNarrow',
-                    //     ),
-                    //   ),
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    Stack(
-                      children: [
-                        Image.asset(
-                          'assets/home/card.png',
-                          width: 330.w,
+                  Stack(
+                    children: [
+                      Image.asset(
+                        'assets/home/card.png',
+                        width: 330.w,
+                      ),
+                      Positioned(
+                        left: 20.w,
+                        top: 15.h,
+                        child: Text(
+                          'Strings',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 45.sp,
+                              color: Colors.white,
+                              fontFamily: 'PragatiNarrow'),
                         ),
-                        Positioned(
-                          left: 20.w,
-                          top: 15.h,
-                          child: Text(
-                            'Arrays',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 45.sp,
-                                color: Colors.white,
-                                fontFamily: 'PragatiNarrow'),
-                          ),
+                      ),
+                      Positioned(
+                        left: 20.w,
+                        top: 60.h,
+                        child: Text(
+                          'Start learning',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25.sp,
+                              color: Colors.white,
+                              fontFamily: 'PragatiNarrow'),
                         ),
-                        Positioned(
-                          left: 20.w,
-                          top: 60.h,
-                          child: Text(
-                            'Start learning',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 25.sp,
-                                color: Colors.white,
-                                fontFamily: 'PragatiNarrow'),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Stack(
-                      children: [
-                        Image.asset(
-                          'assets/home/card.png',
-                          width: 330.w,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Stack(
+                    children: [
+                      Image.asset(
+                        'assets/home/card.png',
+                        width: 330.w,
+                      ),
+                      Positioned(
+                        left: 20.w,
+                        top: 15.h,
+                        child: Text(
+                          'Trees',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 45.sp,
+                              color: Colors.white,
+                              fontFamily: 'PragatiNarrow'),
                         ),
-                        Positioned(
-                          left: 20.w,
-                          top: 15.h,
-                          child: Text(
-                            'Strings',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 45.sp,
-                                color: Colors.white,
-                                fontFamily: 'PragatiNarrow'),
-                          ),
+                      ),
+                      Positioned(
+                        left: 20.w,
+                        top: 60.h,
+                        child: Text(
+                          'Start learning',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25.sp,
+                              color: Colors.white,
+                              fontFamily: 'PragatiNarrow'),
                         ),
-                        Positioned(
-                          left: 20.w,
-                          top: 60.h,
-                          child: Text(
-                            'Start learning',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 25.sp,
-                                color: Colors.white,
-                                fontFamily: 'PragatiNarrow'),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Stack(
-                      children: [
-                        Image.asset(
-                          'assets/home/card.png',
-                          width: 330.w,
-                        ),
-                        Positioned(
-                          left: 20.w,
-                          top: 15.h,
-                          child: Text(
-                            'Trees',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 45.sp,
-                                color: Colors.white,
-                                fontFamily: 'PragatiNarrow'),
-                          ),
-                        ),
-                        Positioned(
-                          left: 20.w,
-                          top: 60.h,
-                          child: Text(
-                            'Start learning',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 25.sp,
-                                color: Colors.white,
-                                fontFamily: 'PragatiNarrow'),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),),),
-    );
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),),);
   }
 }
 
