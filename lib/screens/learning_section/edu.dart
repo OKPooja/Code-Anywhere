35b07717b34a3ecec642c43726f2a1fccc341635
OKPooja <pooja.gawade213046@tsecol.onmicrosoft.com>
2024-02-24 01:50:37 +0530

    problems and learning page added

--- lib/screens/learning_section/edu.dart
+++ lib/screens/learning_section/edu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:project/utils/colors.dart';
 
 class Edu extends StatefulWidget {
   const Edu({super.key});
@@ -8,12 +10,163 @@ class Edu extends StatefulWidget {
 }
 
 class _EduState extends State<Edu> {
+  List<Map<String, String>> dataList = [
+    {
+      'title': 'Arrays',
+      'description':
+      'An array is a data structure with elements of the same memory size..',
+    },
+    {
+      'title': 'Lists',
+      'description':
+      'A list is an ordered collection that allows duplicates and nulls..',
+    },
+    {
+      'title': 'Maps',
+      'description':
+      'A map is an object that maps keys to values. A map cannot contain duplicate keys..',
+    },
+    {
+      'title': 'Sets',
+      'description':
+      'A set is an unordered collection of unique items. Sets do not allow duplicates..',
+    },
+    {
+      'title': 'Queues',
+      'description':
+      'A queue is a collection that orders its elements in a specific order. Queues typically follow the FIFO (First In First Out) order..',
+    },
+    {
+      'title': 'Stacks',
+      'description':
+      'A stack is a collection that follows the LIFO (Last In First Out) principle..',
+    },
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Learn'),
-      ),
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 50.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Video \nTutorial',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 40.sp,
+                    fontFamily: 'PragatiNarrow',
+                    color: Colors.white
+                ),
+              ),
+              Image.asset(
+                'assets/learn/bg.png',
+                width: 370.w,
+                height: 240.h,
+              ),
+              // Padding(
+              //   padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+              //   child: Row(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //     children: [
+              //       // Padding(
+              //       //   padding: EdgeInsets.only(top: 10.h),
+              //       //   child: const Icon(
+              //       //     Icons.arrow_back_ios,
+              //       //     color: Colors.white,
+              //       //     size: 35,
+              //       //   ),
+              //       // ),
+              //       Text(
+              //         'Video \nTutorial',
+              //         style: TextStyle(
+              //           fontWeight: FontWeight.w700,
+              //           fontSize: 36.sp,
+              //           fontFamily: 'PragatiNarrow',
+              //           color: Colors.white
+              //         ),
+              //       )
+              //     ],
+              //   ),
+              // ),
+
+              Container(
+                //height: 550,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(16.r)),
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: dataList.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.w,vertical: 8.h),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.play_circle,
+                                size: 55,
+                                color: Color(0XFF29277A),
+                              ),
+                              SizedBox(width: 10.w),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    dataList[index]['title']!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 36.sp,
+                                      fontFamily: 'PragatiNarrow',
+                                      color: Colors.black87,
+                                      height: 1.2,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width * 0.65,
+                                    child: Text(
+                                      dataList[index]['description']!,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w100,
+                                        fontSize: 15.sp,
+                                        fontFamily: 'Nunito',
+                                        color: Color(0XFF8C8888),
+                                        height: 1.0,
+                                      ),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10.h),
+                          if(index != dataList.length - 1)
+                            const Divider(color: Colors.black87, thickness: 1.0)
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 50.h,),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
--- lib/screens/problems/problems.dart
+++ lib/screens/problems/problems.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:project/utils/colors.dart';
 
 class Problems extends StatefulWidget {
   const Problems({super.key});
@@ -8,12 +10,169 @@ class Problems extends StatefulWidget {
 }
 
 class _ProblemsState extends State<Problems> {
+  List<Map<String, dynamic>> problemDataList = [
+    {
+      'name': 'Largest element in an array',
+      'isCompleted': true,
+      'isBookmarked': false,
+    },
+    {
+      'name': 'Smallest element in an array',
+      'isCompleted': false,
+      'isBookmarked': true,
+    },
+    {
+      'name': 'Merge two sorted arrays',
+      'isCompleted': true,
+      'isBookmarked': true,
+    },
+    {
+      'name': 'Reverse an array',
+      'isCompleted': true,
+      'isBookmarked': false,
+    },
+    {
+      'name': 'Find the missing number in an array',
+      'isCompleted': false,
+      'isBookmarked': false,
+    },
+    {
+      'name': 'Remove duplicates from a sorted array',
+      'isCompleted': true,
+      'isBookmarked': true,
+    },
+    {
+      'name': 'Count frequency of elements in an array',
+      'isCompleted': false,
+      'isBookmarked': false,
+    },
+    {
+      'name': 'Rotate an array',
+      'isCompleted': false,
+      'isBookmarked': true,
+    },
+    {
+      'name': 'Find maximum sum subarray',
+      'isCompleted': true,
+      'isBookmarked': false,
+    },
+    {
+      'name': 'Search in a rotated sorted array',
+      'isCompleted': false,
+      'isBookmarked': true,
+    },
+    {
+      'name': 'Find common elements in multiple arrays',
+      'isCompleted': true,
+      'isBookmarked': false,
+    },
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Problems'),
-      ),
+    return Scaffold(
+        backgroundColor: AppColors.primary,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 50.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    'Problems',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 40.sp,
+                        fontFamily: 'PragatiNarrow',
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 4.w),
+                  child: Text(
+                    'Arrays',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 28.sp,
+                        fontFamily: 'PragatiNarrow',
+                        color: Colors.white),
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: problemDataList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      color: const Color(0xFFECF7FD),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 8.h, horizontal: 10.w),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.6,
+                              child: Text(
+                                problemDataList[index]['name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 22.sp,
+                                  fontFamily: 'PragatiNarrow',
+                                  color: AppColors.primary,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  problemDataList[index]['isCompleted']
+                                      ? Icons.check_circle
+                                      : Icons.circle,
+                                  color: problemDataList[index]['isCompleted']
+                                      ? Colors.green
+                                      : Colors.transparent,
+                                ),
+                                GestureDetector(
+                                  onTap: (){
+                                      setState(() {
+                                            problemDataList[index]['isBookmarked'] = !problemDataList[index]['isBookmarked'];
+                                      });
+                                  },
+                                  child: Icon(
+                                    problemDataList[index]['isBookmarked']
+                                        ? Icons.bookmark
+                                        : Icons.bookmark_border,
+                                    color: problemDataList[index]
+                                    ['isBookmarked']
+                                        ? Colors.black87
+                                        : Colors.black87,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 50.h,),
+              ],
+            ),
+          ),
+        ),
     );
   }
 }
