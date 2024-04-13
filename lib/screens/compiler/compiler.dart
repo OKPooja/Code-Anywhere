9655a790d932e454332888f0a1b9483c28f001b1
OKPooja <poojagawade213046@tsecedu.org>
2024-04-13 15:37:12 +0530

    share plus added

--- lib/screens/compiler/compiler.dart
+++ lib/screens/compiler/compiler.dart
@@ -1,18 +1,14 @@
 import 'package:code_text_field/code_text_field.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-import 'package:highlight/languages/dart.dart';
 import 'package:highlight/languages/python.dart';
 import 'package:highlight/languages/cpp.dart';
 import 'package:highlight/languages/java.dart';
-import 'package:flutter_highlight/themes/monokai-sublime.dart';
-import 'package:flutter_highlight/themes/vs.dart';
-import 'package:flutter_highlight/themes/atom-one-dark.dart';
-import 'package:flutter_highlight/themes/darcula.dart';
 import 'package:project/screens/compiler/syntax_highlight.dart';
-
+import 'package:share_plus/share_plus.dart';
 import '../../api.dart';
 import '../../utils/colors.dart';
 
@@ -61,16 +57,33 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
       enterBottomSheetDuration: const Duration(milliseconds: 300),
     );
   }
+  
+  @override
+  void initState(){
+    _codeController?.addListener(() {});
+    super.initState();
+  }
+
+  _share() async{
+    if (kDebugMode) {
+      print(_codeController!.text);
+    }
+    if(_codeController!.text.isNotEmpty){
+      await Share.share(_codeController!.text);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     var source = '';
     if (_selectedLanguage == 'C++') {
-      source = '#include<iostream> \nusing namespace std; \n\nint main(){ \n cout << "hello"; \n return 0;\n}';
+      source =
+          '#include<iostream> \nusing namespace std; \n\nint main(){ \n  cout << "hello"; \n  return 0;\n}';
     } else if (_selectedLanguage == 'Java') {
-      source = 'import java.util.*;\npublic class Main {\n  public static void main(String[] args) {\n    // Your code here\n    System.out.println("Hello, World!");\n  }\n}';
+      source =
+          'import java.util.*;\npublic class Main {\n  public static void main(String[] args) {\n    // Your code here\n    System.out.println("Hello, World!");\n  }\n}';
     } else if (_selectedLanguage == 'Python') {
-      source = '';
+      source = 'print("Have a great coding experience")';
     }
     _codeController = CodeController(
       text: source,
@@ -78,8 +91,10 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
       patternMap: {
         r'".*"': const TextStyle(color: Colors.white),
         r'[a-zA-Z0-9]+\(.*\)': const TextStyle(color: Colors.white),
-        r'^\s*//.*$': const TextStyle(color: Colors.grey), // Single-line comment
-        r'/\*[\s\S]*?\*/': const TextStyle(color: Colors.grey), // Multi-line comment
+        r'^\s*//.*$':
+            const TextStyle(color: Colors.grey), // Single-line comment
+        r'/\*[\s\S]*?\*/':
+            const TextStyle(color: Colors.grey), // Multi-line comment
       },
       stringMap: theme,
     );
@@ -107,17 +122,18 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
           ),
         ),
         actions: [
-            GestureDetector(
-              onTap: (){
-
-              },
-              child: const Icon(
-                Icons.share,
-                color: Colors.white,
-              ),
+          GestureDetector(
+            onTap: () {
+              _share();
+            },
+            child: const Icon(
+              Icons.share,
+              color: Colors.white,
             ),
+          ),
           PopupMenuButton<String>(
             color: Colors.white,
+            iconColor: Colors.white,
             onSelected: (String value) {
               setState(() {
                 _selectedLanguage = value;
@@ -152,9 +168,17 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
       ),
       body: SingleChildScrollView(
         child: CodeField(
+          onChanged: (String text) {
+            if (text == '(') {
+              _addClosingBracket();
+            }
+          },
           controller: _codeController!,
           cursorColor: AppColors.primary,
-          textStyle: const TextStyle(fontFamily: 'Nunito', fontSize: 20,),
+          textStyle: const TextStyle(
+            fontFamily: 'Nunito',
+            fontSize: 20,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -162,12 +186,13 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
           borderRadius: BorderRadius.circular(100),
         ),
         backgroundColor: AppColors.primary,
-        onPressed: () async{
+        onPressed: () async {
           print("Code:");
           print(_codeController?.text);
           String encodedCode = Uri.encodeComponent(_codeController!.text);
           print(encodedCode);
-          var response = await api.compileCode(encodedCode, "", _selectedLanguage);
+          var response =
+              await api.compileCode(encodedCode, "", _selectedLanguage);
           _showOutputBottomSheet(response);
         },
         child: const Icon(
@@ -176,4 +201,24 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
       ),
     );
   }
+  void _addClosingBracket() {
+      setState(() async {
+        await showDialog<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('That is correct!'),
+                content: const Text('13 is the right answer.'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            });
+      });
+    }
 }
