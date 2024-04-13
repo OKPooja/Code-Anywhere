17599d59b164f561903cda550929f15a5619ef40
OKPooja <poojagawade213046@tsecedu.org>
2024-04-13 17:48:51 +0530

    download and share functionality added

--- lib/screens/compiler/compiler.dart
+++ lib/screens/compiler/compiler.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +10,8 @@ import 'package:highlight/languages/python.dart';
 import 'package:highlight/languages/cpp.dart';
 import 'package:highlight/languages/java.dart';
 import 'package:project/screens/compiler/syntax_highlight.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:toastification/toastification.dart';
 import 'package:share_plus/share_plus.dart';
 import '../../api.dart';
 import '../../utils/colors.dart';
@@ -64,6 +68,79 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
     super.initState();
   }
 
+  Future<String?> getDownloadPath() async {
+    Directory? directory;
+    try {
+      if (Platform.isIOS) {
+        directory = await getApplicationDocumentsDirectory();
+      } else {
+        directory = Directory('/storage/emulated/0/codeAnywhere');
+        if (!await directory.exists()) {
+          await directory.create(recursive: true);
+        }
+      }
+    } catch (err) {
+      if (kDebugMode) {
+        print("Cannot get download folder path $err");
+      }
+    }
+    return directory?.path;
+  }
+  Future<void> requestStoragePermission() async {
+    PermissionStatus status = await Permission.manageExternalStorage.request();
+
+    if (status.isGranted) {
+      if (kDebugMode) {
+        print('Storage permission granted');
+      }
+    } else if (status.isDenied) {
+      if (kDebugMode) {
+        print('Storage permission denied');
+      }
+    } else if (status.isPermanentlyDenied) {
+      if (kDebugMode) {
+        print('Storage permission permanently denied');
+      }
+      openAppSettings();
+    }
+  }
+
+  _download() async {
+    print("Hit download");
+    await requestStoragePermission();
+
+    String? path = await getDownloadPath();
+    if (path != null) {
+      print("Directory: $path");
+      try {
+        File f;
+        if (_selectedLanguage == "C++") {
+          f = File("$path/code.cpp");
+        } else if (_selectedLanguage == "Python") {
+          f = File("$path/code.py");
+        } else {
+          f = File("$path/code.java");
+        }
+        await f.writeAsString(_codeController!.text);
+        print("File saved successfully: ${f.path}");
+        toastification.show(
+          context: context,
+          title: const Text('File saved successfully'),
+          autoCloseDuration: const Duration(seconds: 5),
+          alignment: Alignment.bottomCenter,
+          primaryColor: Colors.green,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          showProgressBar: false,
+        );
+      } catch (e) {
+        print("Error writing file: $e");
+      }
+    } else {
+      print("Download path is null");
+    }
+  }
+
   _share() async{
     if (kDebugMode) {
       print(_codeController!.text);
@@ -83,7 +160,7 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
       source =
           'import java.util.*;\npublic class Main {\n  public static void main(String[] args) {\n    // Your code here\n    System.out.println("Hello, World!");\n  }\n}';
     } else if (_selectedLanguage == 'Python') {
-      source = 'print("Have a great coding experience")';
+      source = 'print("Enjoy coding")';
     }
     _codeController = CodeController(
       text: source,
@@ -106,6 +183,7 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
           icon: const Icon(
             Icons.arrow_back_ios_new,
             color: Colors.white,
+            size: 22,
           ),
           onPressed: () {
             Get.back();
@@ -116,7 +194,7 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
           "Code",
           style: TextStyle(
             fontFamily: 'PragatiNarrow',
-            fontSize: 30.sp,
+            fontSize: 26.sp,
             fontWeight: FontWeight.w500,
             color: Colors.white,
           ),
@@ -124,11 +202,23 @@ class _CodeInputScreenState extends State<CodeInputScreen> {
         actions: [
           GestureDetector(
             onTap: () {
+              _download();
+            },
+            child: const Icon(
+              Icons.file_download_outlined ,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
+          SizedBox(width: 16.w,),
+          GestureDetector(
+            onTap: () {
               _share();
             },
             child: const Icon(
               Icons.share,
               color: Colors.white,
+              size: 22,
             ),
           ),
           PopupMenuButton<String>(
